7f8eaa7180841f9b3bed8f78cf5fbf36641dd23c
Juan Daniel Perez <juan.perez@cencosudscotiabank.cl>
2022-01-24 15:22:59 -0500

    Empty line added to the file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,4 @@ class User < ActiveRecord::Base
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
     include DeviseTokenAuth::Concerns::User
   end
+  
