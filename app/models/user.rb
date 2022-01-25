38bd64cc4b8bf0bb6d80cfd60177de2132855731
Juan Daniel Perez <juan.perez@cencosudscotiabank.cl>
2022-01-25 12:27:29 -0500

    Adding the rent model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
-class User < ActiveRecord::Base
-    extend Devise::Models
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-    include DeviseTokenAuth::Concerns::User
-  end
+class User < ApplicationRecord
+  has_many :rents, dependent: :destroy
+  extend Devise::Models
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
